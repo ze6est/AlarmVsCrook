7d20d2e20144a61198fe00b8214af6c28580e29d
Roman Kopylov <ze6est1@yandex.ru>
2023-11-25 23:10:55 +0300

    Init

---
+++ Assets/CodeBase/Control.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            Move(Vector3.forward);
+
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            Move(Vector3.back);
+    }
+
+    private void Move(Vector3 direction) => 
+        transform.Translate(direction * _speed * Time.deltaTime);
+}
